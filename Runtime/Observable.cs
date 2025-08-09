b745d0077c14405c2ae6c6baef4c7b10559b4432
Sebastian Baier <sebastian.baier93@hotmail.de>
2025-08-09 18:20:39 +0200

    Setups package. Adds observable value and tests

---
+++ Runtime/Observable.cs
@@ -0,0 +1,33 @@
+namespace Calluna
+{
+    public class Observable<T> : ReadonlyObservable<T>
+    {
+        public event ValueChangedWithValues OnChangedWithValues;
+        public event ValueChanged OnChanged;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                T former = _value;
+                _value = value;
+                OnChangedWithValues?.Invoke(former, _value);
+                OnChanged?.Invoke();
+            }
+        }
+
+        private T _value;
+
+        public void SetValueWithoutNotify(T newValue)
+        {
+            _value = newValue;
+        }
+        
+        public static implicit operator Observable<T>(T value) => new(){_value = value};
+        public static implicit operator T(Observable<T> value) => value.Value;
+        
+        public delegate void ValueChangedWithValues(T formerValue, T newValue);
+        public delegate void ValueChanged();
+    }
+}
