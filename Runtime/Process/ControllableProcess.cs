5c0820fc92f8ec0217640e49e06eeae4c0f0daf2
Sebastian Baier <sebastian.baier93@hotmail.de>
2025-10-13 23:59:07 +0200

    Removes process logic and moves it into its own package

--- Runtime/Process/ControllableProcess.cs
+++
@@ -1,9 +0,0 @@
-namespace Calluna.Process
-{
-    public interface ControllableProcess : Process
-    {
-        public void Start();
-        public void Tick();
-        public void Abort();
-    }
-}
