f665933c95e00dd88bd2f83605782beebba359f9
Sebastian Baier <sebastian.baier93@hotmail.de>
2026-01-02 17:17:35 +0100

    Readds id to scriptableobjectids. Adds string comparison to scriptableobjectids.

--- Runtime/Core/StringId/ScriptableObjectId.cs
+++ Runtime/Core/StringId/ScriptableObjectId.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 
 namespace Calluna
 {
-    public abstract class ScriptableObjectId : ScriptableObject, IEquatable<ScriptableObjectId>
+    public abstract class ScriptableObjectId : ScriptableObject,
+        IEquatable<ScriptableObjectId>, IEquatable<string>
     {
-        [field: SerializeField] public string Descriptor { get; private set; }
+        [field: SerializeField] public string Id { get; private set; }
 
         public bool Equals(ScriptableObjectId other)
         {
             return ReferenceEquals(this, other);
         }
 
+        public bool Equals(string other)
+        {
+            return Id == other;
+        }
+
         public override bool Equals(object obj)
         {
-            return ReferenceEquals(this, obj);
+            if (ReferenceEquals(this, obj)) return true;
+            if (ReferenceEquals(null, obj)) return false;
+            if (obj is string stringObj && Id == stringObj) return true;
+            return false;
         }
 
         public override int GetHashCode()
@@ -27,14 +36,24 @@ namespace Calluna
             return left?.Equals(right) ?? ReferenceEquals(null, right);
         }
 
+        public static bool operator ==(ScriptableObjectId left, string right)
+        {
+            return left?.Equals(right) ?? ReferenceEquals(null, right);
+        }
+
         public static bool operator !=(ScriptableObjectId left, ScriptableObjectId right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator !=(ScriptableObjectId left, string right)
         {
             return !(left == right);
         }
 
         public override string ToString()
         {
-            return Descriptor;
+            return Id;
         }
     }
 }
