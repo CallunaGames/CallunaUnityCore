d855ae1e6cab4a5130d884d1d29218586f843305
Sebastian Baier <sebastian.baier93@hotmail.de>
2026-02-13 17:43:36 +0100

    Adds tween type

---
+++ Samples~/TweenSample/SelectedTweenTester.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Calluna.Core.Samples.TweenSample
+{
+    public class SelectedTweenTester : TweenTester
+    {
+        [SerializeField] private TweenType _tweenType;
+        
+        protected override float GetTweenValue(float value)
+        {
+            return Tween.GetEaseFunction(_tweenType).Invoke(value);
+        }
+    }
+}
