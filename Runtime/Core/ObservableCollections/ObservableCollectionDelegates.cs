c6b94f6efec6c8ca9f042328edcded97adc1bfd4
Sebastian Baier <sebastian.baier93@hotmail.de>
2026-02-15 14:53:57 +0100

    Fixes naming of ItemSwapEvent arguments

--- Runtime/Core/ObservableCollections/ObservableCollectionDelegates.cs
+++ Runtime/Core/ObservableCollections/ObservableCollectionDelegates.cs
@@ -2,5 +2,5 @@ namespace Calluna
 {
     public delegate void ItemChangeEvent<in TValue>(TValue item, int index);
     public delegate void ItemReplaceEvent<in TValue>(TValue nextItem, TValue formerItem, int index);
-    public delegate void ItemSwapEvent<in TValue>(TValue newIndex1Item, int index1, TValue item2, int newIndex2Item);
+    public delegate void ItemSwapEvent<in TValue>(TValue newIndex1Item, int index1, TValue newIndex2Item, int index2);
 }
